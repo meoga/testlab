4fff9082a0687021c1f6f5aef3f0d53f067a98e5
meoga <jijohn.vega@gmail.com>
2023-04-15 21:53:16 +0900

    Stack.h 수정

--- Stack.h
+++ Stack.h
@@ -3,9 +3,6 @@
 
 //1. 템플릿 클래스로 확장해야함
 //2. Stack형식으로 Delete 함수 재정의해야함
-//주의: first, current_size는 class의 멤버 변수이기 때문에 this 포인터를 사용하여 가져와야함
-
-//LinkedList class를 상속받음
 class Stack : public LinkedList{
 	public:
 		bool Delete (int &element){
